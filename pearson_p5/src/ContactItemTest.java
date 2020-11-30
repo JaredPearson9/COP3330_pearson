bc3f6b5a28d54683e3c975403a4085a7e459c652
JaredPearson9 <jaredp999@yahoo.com>
2020-11-29 20:51:49 -0500

    Version 1

---
+++ pearson_p5/src/ContactItemTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContactItemTest {
+/*
+    creationFailsWithAllBlankValues()
+    creationSucceedsWithBlankEmail()
+    creationSucceedsWithBlankFirstName()
+    creationSucceedsWithBlankLastName()
+    creationSucceedsWithBlankPhone()
+    creationSucceedsWithNonBlankValues()
+    editingFailsWithAllBlankValues()
+    editingSucceedsWithBlankEmail()
+    editingSucceedsWithBlankFirstName()
+    editingSucceedsWithBlankLastName()
+    editingSucceedsWithBlankPhone()
+    editingSucceedsWithNonBlankValues()
+    testToString()
+*/
+}
---
+++ pearson_p5/src/ContactListTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContactListTest {
+/*
+    addingItemsIncreasesSize()
+    editingItemsFailsWithAllBlankValues()
+    editingItemsFailsWithInvalidIndex()
+    editingSucceedsWithBlankFirstName()
+    editingSucceedsWithBlankLastName()
+    editingSucceedsWithBlankPhone()
+    editingSucceedsWithNonBlankValues()
+    newListIsEmpty()
+    removingItemsDecreasesSize()
+    removingItemsFailsWithInvalidIndex()
+    savedContactListCanBeLoaded()
+*/
+}
