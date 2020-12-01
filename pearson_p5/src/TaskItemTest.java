428c2d5b1207744992fcaf0acae9f2cb166120cd
JaredPearson9 <jaredp999@yahoo.com>
2020-11-30 19:30:49 -0500

    Version 3

--- pearson_p5/src/TaskItemTest.java
+++ pearson_p5/src/TaskItemTest.java
@@ -25,7 +25,12 @@ public class TaskItemTest {
 
     @Test
     public void editingDescriptionSucceedsWithExpectedValue(){
+        TaskList list = new TaskList();
+        TaskItem task = new TaskItem("Title","DescriptionBefore","2020-05-25",false);
+        list.add(task);
 
+        assertDoesNotThrow(() -> list.edit(0,new TaskItem("Title","Description","2020-05-25",false)));
+        assertEquals(list.tasks.get(0).getTaskItemDescription(),"Description");
     }
 
     @Test
@@ -38,16 +43,35 @@ public class TaskItemTest {
     }
 
     @Test
-    public void editingTaskItemDueDateSucceedsWithValidDate(){
+    public void editingTaskItemDueDateFailsWithInvalidDateYYYMMDD(){
+        TaskList list = new TaskList();
+        TaskItem task = new TaskItem("Title","Description","2002-05-20",false);
+        list.add(task);
+
+        assertThrows(TaskItem.InvalidDueDateException.class , () -> list.edit(0,new TaskItem("Title","Description","202-0-20",false)));
+    }
+
+    @Test
+    public void editingTaskItemDueDateSucceedsWithExpectedDueDate(){
         TaskList list = new TaskList();
         TaskItem task = new TaskItem("Title","Description","2020-05-25",false);
         list.add(task);
 
         assertDoesNotThrow(() -> list.edit(0,new TaskItem("Title","Description","2020-06-20",false)));
+        assertEquals(list.tasks.get(0).getTaskItemDueDate(),"2020-06-20");
     }
 
     @Test
     public void editingTaskItemTitleFailsWithInvalidTitle(){
+        TaskList list = new TaskList();
+        TaskItem task = new TaskItem("Title","Description","2020-05-25",false);
+        list.add(task);
+
+        assertThrows(TaskItem.InvalidTitleException.class , () -> list.edit(0, new TaskItem("","Description","2020-06-20",false)));
+    }
+
+    @Test
+    public void editingTaskItemTitleFailsWithEmptyString(){
         TaskList list = new TaskList();
         TaskItem task = new TaskItem("Title","Description","2020-05-25",false);
         list.add(task);
